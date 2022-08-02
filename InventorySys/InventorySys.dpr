c7638cfef7f70006bdbb84356fee769b516de7b6
xiaowuwang <xiaowuwang@users.noreply.github.com>
2022-08-02 18:27:54 +1000

    REMOVE USELESS

--- InventorySys/InventorySys.dpr
+++ InventorySys/InventorySys.dpr
@@ -9,14 +9,11 @@ uses
   UnitOrderItem in 'UnitOrderItem.pas',
   UnitDataInterfaces in 'UnitDataInterfaces.pas',
   UnitInventory in 'UnitInventory.pas',
-  Com_DBHelper in '..\Common\Com_DBHelper.pas',
-  Com_DBUtil in '..\Common\Com_DBUtil.pas',
   Com_Exception in '..\Common\Com_Exception.pas',
-  Com_OSUtil in '..\Common\Com_OSUtil.pas',
-  Com_Streams in '..\Common\Com_Streams.pas',
   com_sync in '..\Common\com_sync.pas',
   UnitDBXMetadataHelper in '..\Common\UnitDBXMetadataHelper.pas',
-  UnitDSServerDB in '..\Common\UnitDSServerDB.pas';
+  UnitDSServerDB in '..\Common\UnitDSServerDB.pas',
+  Com_Streams in '..\Common\Com_Streams.pas';
 
 {$R *.res}
 
