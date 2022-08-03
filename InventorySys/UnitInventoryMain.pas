efe6e15dfcf8c6776e4d0490a4883533f110b69e
xiaowuwang <xiaowuwang@users.noreply.github.com>
2022-08-03 17:15:20 +1000

    Add sqlite connection

--- InventorySys/UnitInventoryMain.pas
+++ InventorySys/UnitInventoryMain.pas
@@ -6,12 +6,19 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DBXFirebird,
   Data.DB, Data.SqlExpr, UnitInventory, UnitDataInterfaces, Vcl.StdCtrls,
-  Vcl.ExtCtrls;
+  Vcl.ExtCtrls, Data.DbxSqlite, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  FireDAC.Comp.Client, FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteDef,
+  FireDAC.Phys.SQLite;
 
 type
   TFormInventoryMain = class(TForm)
     Panel1: TPanel;
     ButtonAddProduct: TButton;
+    SQLConnection1: TSQLConnection;
+    FDConnection1: TFDConnection;
+    FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink;
     procedure FormCreate(Sender: TObject);
     procedure ButtonAddProductClick(Sender: TObject);
   private
@@ -33,8 +40,8 @@ uses UnitFirebird, UnitProduct, UnitSQLite;
 
 procedure TFormInventoryMain.FormCreate(Sender: TObject);
 begin
-  FConn := TInventoryFB.Create;
-//  FConn := TInventorySQLite.Create;
+//  FConn := TInventoryFB.Create;
+  FConn := TInventorySQLite.Create;
   FConn.ConnectToDB;
   FInventory := TInventory.create(FConn);
 end;
--- InventorySys/UnitSQLite.pas
+++ InventorySys/UnitSQLite.pas
@@ -2,13 +2,22 @@ unit UnitSQLite;
 
 interface
 
-uses UnitDataInterfaces, Data.SqlExpr;
+uses SysUtils, UnitDataInterfaces, Data.SqlExpr, Data.DbxSqlite, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  FireDAC.Comp.Client, FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteDef,
+  FireDAC.Phys.SQLite, UnitDBXMetadataHelper,
+  DbxCommon, DbxMetaDataProvider, DBXDataExpressMetaDataProvider,
+  DbxInterbase, DbxClient, Dialogs, Data.DBXFirebird, Winapi.ShlObj;
 
 type
 
   TInventorySQLite = class(TInterfacedObject, IDataConnection)
   private
-    FConn: TSQLConnection;
+    FDrvLnk: TFDPhysSQLiteDriverLink;
+    FFDConn: TFDConnection;
+    FConn  : TSQLConnection;
+    procedure CreateSchema;
   public
     constructor Create;
     function ConnectToDB : Boolean;
@@ -21,12 +30,135 @@ uses UnitDSServerDB;
 constructor TInventorySQLite.Create;
 begin
   inherited create;
-  FConn := TSQLConnection.Create(nil);
+  FDrvLnk := TFDPhysSQLiteDriverLink.Create(nil);
+  FFDConn := TFDConnection.Create(nil);
+  FConn   := TSQLConnection.Create(nil);
 end;
 
 function TInventorySQLite.ConnectToDB;
 begin
-  InitialConnection(FConn, DB_NAME);
+  if not FileExists('InventorySqlite.sqlite') then
+  begin
+    FFDConn.DriverName:='Sqlite';
+    FFDConn.Params.Values['database']:='InventorySqlite.sqlite';
+    try
+      FFDConn.Connected := true;
+    except
+    end;
+    FFDConn.Connected := false;
+    FConn.DriverName:='Sqlite';
+    FConn.Params.Values['database']:='InventorySqlite.sqlite';
+    FConn.Connected := true;
+    CreateSchema;
+//    PopulateData(FConn);
+  end;
 end;
+
+procedure TInventorySQLite.CreateSchema;
+var
+  Provider    : TDBXDataExpressMetaDataProvider;
+  ATable      : TDBXMetaDataTable;
+begin
+  Provider := DBXGetMetaProvider(FConn.DBXConnection);
+  //CUSTOMER
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'CUSTOMER';
+  AddInt32Column         (ATable, 'CUSTOMERID');
+  AddUnicodeVarCharColumn(ATable, 'FIRSTNAME', 20);
+  AddUnicodeVarCharColumn(ATable, 'LASTNAME', 20);
+  AddUnicodeVarCharColumn(ATable, 'GENDER', 20);
+  AddUnicodeVarCharColumn(ATable, 'EMAILADDRESS', 20);
+  AddUnicodeVarCharColumn(ATable, 'ADDRESS1', 20);
+  AddUnicodeVarCharColumn(ATable, 'ADDRESS2', 20);
+  AddUnicodeVarCharColumn(ATable, 'CITY', 20);
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  Provider.CreateTable(ATable);
+  //PRODUCT
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'PRODUCT';
+  AddInt32Column         (ATable, 'PRODUCTID');
+  AddUnicodeVarCharColumn(ATable, 'NAME', 20);
+  AddUnicodeVarCharColumn(ATable, 'DESCRIPTION', 50);
+  AddDoubleColumn        (ATable, 'PRICE');
+  AddInt8Column          (ATable, 'TAXTYPE');
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  Provider.CreateTable(ATable);
+  //ORDER
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'ORDERS';
+  AddInt32Column         (ATable, 'ORDERID');
+  AddInt32Column         (ATable, 'CUSTOMERID');
+  AddUnicodeVarCharColumn(ATable, 'ORDERDATE',20);
+  AddInt8Column          (ATable, 'STATUS');
+  AddInt8Column          (ATable, 'PAYMENTTYPE');
+  AddUnicodeVarCharColumn(ATable, 'TRACKINGNUMBER', 20);
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  Provider.CreateTable(ATable);
+  //ORDERITEM
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'ORDERITEM';
+  AddInt32Column         (ATable, 'ORDERITEMID');
+  AddInt32Column         (ATable, 'ORDERID');
+  AddInt8Column          (ATable, 'ORDERLINE');
+  AddInt32Column         (ATable, 'PRODUCTID');
+  AddInt32Column         (ATable, 'QUANTITY');
+  AddDoubleColumn        (ATable, 'UNITPRICE');
+  AddInt8Column          (ATable, 'TAXTYPE');
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  Provider.CreateTable(ATable);
+  //EMPLOYEES
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'EMPLOYEE';
+  AddInt32Column         (ATable, 'EMPLOYEEID');
+  AddInt8Column          (ATable, 'STORENO');
+  AddInt32Column         (ATable, 'STOREEMPLOYEEID');
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  AddInt8Column          (ATable, 'TYPEID');
+  AddInt8Column          (ATable, 'STATUSID');
+  AddUnicodeVarCharColumn(ATable, 'CODE', 10);
+  AddUnicodeVarCharColumn(ATable, 'FIRSTNAME', 30);
+  AddUnicodeVarCharColumn(ATable, 'LASTNAME', 30);
+  AddUnicodeVarCharColumn(ATable, 'EMAIL', 30);
+  AddUnicodeVarCharColumn(ATable, 'PHONE', 30);
+  AddInt32Column         (ATable, 'ADDRESSID');
+  AddInt8Column          (ATable, 'DEPARTMENTID');
+  AddUnicodeVarCharColumn(ATable, 'PASSWORD', 30);
+  AddUnicodeVarCharColumn(ATable, 'DOB',20);
+  AddDoubleColumn        (ATable, 'SALARY');
+  AddInt8Column          (ATable, 'GENDER');
+  Provider.CreateTable(ATable);
+  //MODROLES
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'MODROLE';
+  AddInt32Column         (ATable, 'MODROLEID');
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  AddInt8Column          (ATable, 'TYPEID');
+  AddInt8Column          (ATable, 'STATUSID');
+  AddUnicodeVarCharColumn(ATable, 'APPLYTO', 30);
+  AddUnicodeVarCharColumn(ATable, 'AUTHROLE', 30);
+  AddUnicodeVarCharColumn(ATable, 'DENIEDROLE', 30);
+  Provider.CreateTable(ATable);
+  //USERROLES
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'USERROLE';
+  AddInt32Column         (ATable, 'USERROLEID');
+  AddUnicodeVarCharColumn(ATable, 'ADDEDON',20);
+  AddInt8Column          (ATable, 'TYPEID');
+  AddInt8Column          (ATable, 'STATUSID');
+  AddUnicodeVarCharColumn(ATable, 'USERS', 30);
+  AddUnicodeVarCharColumn(ATable, 'ROLES', 30);
+  Provider.CreateTable(ATable);
+  // Log
+  ATable := TDBXMetaDataTable.Create;
+  ATable.TableName := 'LOG';
+  AddInt32Column         (ATable, 'LOGID');
+  AddUnicodeVarCharColumn(ATable, 'IP_ADDRESS', 20);
+  AddUnicodeVarCharColumn(ATable, 'EVENT', 50);
+  AddUnicodeVarCharColumn(ATable, 'CREATED',20);
+  Provider.CreateTable(ATable);
+  FreeAndNil(Provider);
+  FreeAndNil(ATable);
+end;
+
 
 end.
