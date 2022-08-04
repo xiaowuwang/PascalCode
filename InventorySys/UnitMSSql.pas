f87475633d1c68bb5b89b6a087c2dba3fd601dad
xiaowuwang <xiaowuwang@users.noreply.github.com>
2022-08-04 21:40:32 +1000

    Add MSSql connection

--- InventorySys/UnitMSSql.pas
+++ InventorySys/UnitMSSql.pas
@@ -8,73 +8,140 @@ uses SysUtils, UnitDataInterfaces, Data.DbxSqlite, FireDAC.Stan.Intf, FireDAC.St
   FireDAC.Comp.Client, FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteDef,
   FireDAC.Phys.SQLite, FireDAC.Phys.MSSQL, FireDAC.Phys.MSSQLDef,
   FireDAC.Phys.ODBCBase, FireDAC.Comp.ScriptCommands, FireDAC.Stan.Util,
-  FireDAC.Comp.Script;
+  FireDAC.Comp.Script, Data.DB;
 
 type
 
   TInventoryMSSql = class(TInterfacedObject, IDataConnection)
   private
-    FDConn: TFDConnection;
+    FFDConn: TFDConnection;
     FDPhysMSSQLDrvLnk: TFDPhysMSSQLDriverLink;
     FDScript : TFDScript;
+    FQuery   : TFDQuery;
+    function DatabaseNotExists:Boolean;
     procedure CreateDatabase;
     procedure CreateSchema;
   public
     constructor Create;
     function ConnectToDB : Boolean;
+    property FDConn : TFDConnection read FFDConn;
   end;
 
 implementation
 
+uses Com_Exception;
+
 constructor TInventoryMSSql.Create;
 begin
   inherited create;
-  FDConn := TFDConnection.Create(nil);
-  FDConn.Params.Add('DriverID=MSSQL');
-  FDConn.Params.Add('Server=STEVENLAPTOP');
-  FDConn.Params.Add('Database=master');
-  FDConn.Params.Add('User_Name=stevenwang');
-  FDConn.Params.Add('Password=W5Passw0rd');
-  FDConn.Connected := True;
+  FDPhysMSSQLDrvLnk:= TFDPhysMSSQLDriverLink.Create(nil);
+  FFDConn := TFDConnection.Create(nil);
+  FFDConn.Params.Add('DriverID=MSSQL');
+  FFDConn.Params.Add('Server=STEVENLAPTOP');
+  FFDConn.Params.Add('User_Name=stevenwang');
+  FFDConn.Params.Add('Password=W5Passw0rd');
+  FFDConn.Connected := True;
 end;
 
+function TInventoryMSSql.DatabaseNotExists:Boolean;
+begin
+  FQuery := TFDQuery.Create(Nil);
+  FQuery.Connection := FDConn;
+  FQuery.SQL.Text := 'SELECT name FROM master.dbo.sysdatabases WHERE name = N''New_Database'' ';
+  FQuery.Active := True;
+  result := (FQuery.RecordCount = 0);
+
+end;
 function TInventoryMSSql.ConnectToDB;
 begin
-  CreateDatabase;
+  if DatabaseNotExists then
+  begin
+    CreateDatabase;
+    CreateSchema;
+  end
+  else
+  begin
+    FFDConn.Connected :=False;
+    FFDConn.Params.Clear;
+    FFDConn.Params.Add('DriverID=MSSQL');
+    FFDConn.Params.Add('Server=STEVENLAPTOP');
+    FFDConn.Params.Add('User_Name=stevenwang');
+    FFDConn.Params.Add('Password=W5Passw0rd');
+    FFDConn.Params.Add('Database=New_Database');
+    FFDConn.Connected := True;
+  end;
 end;
 
 procedure TInventoryMSSql.CreateDatabase;
 begin
-  FDScript := TFDScript.Create(nil);
-  FDScript.Connection := FDConn;
-  FDScript.ScriptOptions.CommandSeparator := 'go';
-  with FDScript do
+  FQuery := TFDQuery.Create(Nil);
+  FQuery.Connection := FDConn;
+  FQuery.SQL.Text := 'SELECT name FROM master.dbo.sysdatabases WHERE name = N''New_Database'' ';
+  FQuery.Active := True;
+  If FQuery.RecordCount= 0 then
   begin
-    SQLScripts.Clear;
-    SQLScripts.Add;
-    with SQLScripts[0].SQL do
+    FDScript := TFDScript.Create(nil);
+    FDScript.Connection := FFDConn;
+    FDScript.ScriptOptions.CommandSeparator := 'go';
+    with FDScript do
     begin
-      Add('IF EXISTS');
-      Add('(');
-      Add('SELECT name FROM master.dbo.sysdatabases');
-      Add('WHERE name = N''New_Database''');
-      Add(')');
-      Add('BEGIN');
-      Add('SELECT ''Database Name already Exist'' AS Message');
-      Add('END');
-      Add('ELSE');
-      Add('BEGIN');
-      Add('CREATE DATABASE [New_Database]');
-      Add('SELECT ''New Database is Created''');
-      Add('END');
+      SQLScripts.Clear;
+      SQLScripts.Add;
+      with SQLScripts[0].SQL do
+      begin
+        Add('IF EXISTS');
+        Add('(');
+        Add('    SELECT name FROM master.dbo.sysdatabases');
+        Add('    WHERE name = N''New_Database''');
+        Add(')');
+        Add('BEGIN');
+        Add('    SELECT ''Database Name already Exist'' AS Message');
+        Add('END');
+        Add('ELSE');
+        Add('BEGIN');
+        Add('    CREATE DATABASE [New_Database]');
+        Add('END');
+      end;
+      ValidateAll;
+      ExecuteAll;
     end;
-    ValidateAll;
-    ExecuteAll;
   end;
 end;
 
 procedure TInventoryMSSql.CreateSchema;
+var
+  Table: TFDTable;
 begin
+  FFDConn.Connected :=False;
+  FFDConn.Params.Clear;
+  FFDConn.Params.Add('DriverID=MSSQL');
+  FFDConn.Params.Add('Server=STEVENLAPTOP');
+  FFDConn.Params.Add('User_Name=stevenwang');
+  FFDConn.Params.Add('Password=W5Passw0rd');
+  FFDConn.Params.Add('Database=New_Database');
+  FFDConn.Connected := True;
+  Table := TFDTable.Create(nil);
+  try
+    Table.Connection := FDConn;
+    { specify table name }
+    Table.TableName := 'CUSTOMER';
+    { add some fields }
+    Table.FieldDefs.Add('CUSTOMERID', ftInteger, 0, False);
+    Table.FieldDefs.Add('FIRSTNAME', ftString, 50, False);
+    Table.FieldDefs.Add('LASTNAME', ftString, 50, False);
+    Table.FieldDefs.Add('GENDER', ftString, 50, False);
+    Table.FieldDefs.Add('EMAILADDRESS', ftString, 50, False);
+    Table.FieldDefs.Add('ADDRESS1', ftString, 50, False);
+    Table.FieldDefs.Add('ADDRESS2', ftString, 50, False);
+    Table.FieldDefs.Add('CITY', ftString, 50, False);
+    Table.FieldDefs.Add('ADDEDON', ftDateTime, 0, False);
+    { define primary key index }
+    Table.AddIndex('pkMyTableID', 'CUSTOMERID', '', [soPrimary]);
+    { and create it; when the first parameter is True, an existing one is dropped }
+    Table.CreateTable(False);
+  finally
+    FreeAndNil(Table);
+  end;
 end;
 
 end.
